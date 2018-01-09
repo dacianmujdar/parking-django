0750f81fa3cacbf4e0e4cdc5150565508044ab04
Dacian Mujdar <dacian.mujdar@rodeapps.com>
2018-01-09 14:04:40 +0200

    stuff

--- parking_project/urls.py
+++ parking_project/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from parking.views import ParkingList
+from parking_project.parking.views import ParkingList
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
