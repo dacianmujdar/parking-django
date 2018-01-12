b33206666d725769bf5cfe573df9896ba9a8894d
Dacian Mujdar <dacian.mujdar@rodeapps.com>
2018-01-12 10:45:30 +0200

    added cors

--- parking_project/settings.py
+++ parking_project/settings.py
@@ -39,12 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'parking_project.parking',
-    'oauth2_provider'
+    'oauth2_provider',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
