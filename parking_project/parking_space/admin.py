2b9145e35d47afbfb961d6c07c272cb806c28039
MadalinaDinga <madadinga@gmail.com>
2018-05-27 13:02:57 +0300

    * updated Pillow version (compat with Python3.6) + model imports

--- parking_project/parking_space/admin.py
+++ parking_project/parking_space/admin.py
@@ -1,5 +1,4 @@
-from models import ParkingSpace
+from parking_project.parking_space.models import ParkingSpace
 from django.contrib import admin
-
 
 admin.site.register(ParkingSpace)
--- parking_project/requests/admin.py
+++ parking_project/requests/admin.py
@@ -1,5 +1,4 @@
-from models import Request
+from parking_project.requests.models import Request
 from django.contrib import admin
-
 
 admin.site.register(Request)
