02e55d22739d385ab152b49c6fed353d2989706d
Dacian Mujdar <dacian.mujdar@rodeapps.com>
2018-06-14 18:36:37 +0300

    add owner to parking space

--- parking_project/parking_space/serializers.py
+++ parking_project/parking_space/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
+
+from parking_project.account.serializers import AccountSerializer
 from parking_project.parking_space.models import ParkingSpace
 
 
 class ParkingSpaceSerializer(serializers.ModelSerializer):
+    allocated_to = AccountSerializer(read_only=True)
+
     class Meta:
         model = ParkingSpace
-        fields = ('code', 'is_occupied', 'times_extended', 'start_date', 'expiration_date')
+        fields = ('code', 'is_occupied', 'times_extended', 'start_date', 'expiration_date', 'allocated_to')
