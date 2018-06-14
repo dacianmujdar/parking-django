8791ff8a3835e72cef984de79a4584b86a02a7cc
Dacian Mujdar <dacian.mujdar@rodeapps.com>
2018-06-14 18:52:27 +0300

    asd

--- parking_project/offer/serializers.py
+++ parking_project/offer/serializers.py
@@ -4,5 +4,10 @@ from parking_project.offer.models import Offer
 
 
 class OfferSerializer(serializers.ModelSerializer):
+    parking_code = serializers.SerializerMethodField()
+
     class Meta:
         model = Offer
+
+    def get_parking_code(self, offer):
+        return offer.parking_space.code
--- parking_project/requests/serializers.py
+++ parking_project/requests/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
+from parking_project.offer.serializers import OfferSerializer
 from parking_project.requests.models import Request
 
 
 class RequestSerializer(serializers.ModelSerializer):
+    offer = OfferSerializer(read_only=True)
+
     class Meta:
         model = Request
 
