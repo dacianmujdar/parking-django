c487ffe12a16ab3e48090c3a89af49bcf222e281
Dacian Mujdar <dacian.mujdar@rodeapps.com>
2018-06-14 18:40:51 +0300

    asd

--- parking_project/requests/models.py
+++ parking_project/requests/models.py
@@ -6,20 +6,11 @@ class Request(models.Model):
     """
     Request model
     """
-    REQUEST_TYPES = Choices(
-        ('ASK_FOR_SUBSCRIPTION', 1, 'Ask for subscription'),
-        ('REQUEST_OFFER', 2, 'Request an offer')
-    )
-
     REQUEST_STATUS = Choices(
         ('PENDING', 1, 'Pending'),
         ('ACCEPTED', 2, 'Accepted'),
         ('REJECTED', 3, 'Rejected')
     )
-
-    request_type = models.PositiveSmallIntegerField(choices=REQUEST_TYPES, verbose_name='request type',
-                                                    help_text='The type of request')
-
     status = models.PositiveSmallIntegerField(choices=REQUEST_STATUS, verbose_name='status',
                                               default=REQUEST_STATUS.PENDING)
     offer = models.ForeignKey('offer.Offer', related_name='requests')
